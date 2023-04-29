c2d99c94572e1ed06eb662edd60d05a980b3fc73
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 00:27:21 +0530

    Done

---
+++ ftweb27_arrays/problem4.js
@@ -0,0 +1,9 @@
+let class_room= ["luffy",1,2,3, true, false];
+
+
+console.log(class_room[0]);
+console.log(class_room[1]);
+console.log(class_room[2]);
+console.log(class_room[3]);
+console.log(class_room[4]);
+console.log(class_room[5]);
