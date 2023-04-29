6cb74963814e63676b82c3dddb5fa0b314e39a95
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 02:35:05 +0530

    Done

---
+++ ftweb27string/problem7.js
@@ -0,0 +1,17 @@
+let a= [1,2,3];
+
+// console.log(a[0])//on the basis of index
+
+let b= "hi everyone";
+
+// console.log(b[1])// i
+
+// console.log(b[3]);//e
+
+// console.log(b[2]);
+
+// create a string, run loop print each and every character in it.
+
+for(let i=0;i<b.length;i++){
+  console.log(b[i]);
+}
