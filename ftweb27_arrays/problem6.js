fbda2b1b97763905d1fc98100394af3dcd0fc6a0
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 00:27:50 +0530

    Done

---
+++ ftweb27_arrays/problem6.js
@@ -0,0 +1,8 @@
+let class_room= ["luffy",1,2,3, true, false,"zoro","sanji","nami"];
+
+class_room.length// the entire length of array /or strenght of array
+
+for(let i=0;i<class_room.length;i++){
+  console.log(class_room[i])
+}
+
