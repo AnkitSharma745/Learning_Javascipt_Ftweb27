89f476cd356e3c1bf18d12931ccec1666338e206
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 02:54:32 +0530

    MY learning has done

---
+++ ftweb27_objects2/problem7.js
@@ -0,0 +1,10 @@
+let a= "123";
+
+// Number(a)// it will convert a string to a number.
+// console.log(typeof(a));
+// console.log(typeof(Number(a)));
+
+
+let b= "masai";
+
+console.log(Number(b));
