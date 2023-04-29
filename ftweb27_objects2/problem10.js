2466617d810936ab3ef76e405fb35055bdc21d78
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 02:55:20 +0530

    MY learning has done

---
+++ ftweb27_objects2/problem10.js
@@ -0,0 +1,16 @@
+let student= ["goku", "gohan", "piccalo", "chichi"];
+
+let score= [40,34,43,67];
+
+//convert this above two arrays into a below format.
+
+// [
+//   {name: "goku", score: 40},
+//   {name: "gohan", score:34},
+//   {name:"piccalo", score:43},
+//   {name: "chici", score:67}  
+// ]
+
+//home work part -->
+//1 . empty array.
+// 2. empty object
