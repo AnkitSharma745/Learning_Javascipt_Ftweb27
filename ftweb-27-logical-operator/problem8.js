848b5fa94f96c9154666003d7e76f52ae9be1336
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-29 13:15:42 +0530

    done

---
+++ ftweb-27-logical-operator/problem8.js
@@ -0,0 +1,14 @@
+let weather_app= "anbadfsdfsd";
+
+switch(weather_app){
+
+  case "sunny": console.log("it is beautiful day");
+    break;
+  case "cloudy": console.log("it is a bit cloudy");
+    break;
+  case "rainy": console.log("it is raining outside");
+    break;
+  default: console.log("sorry, we do not have current weather information");
+}
+
+// it is simillar to just if--else..if -- else statement.
