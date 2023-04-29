2b74205e178a8a1e526389cf01e74354942f483f
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-29 12:55:34 +0530

    Problem1 has been done

---
+++ ftweb-27-conditional statements/problem1.js
@@ -0,0 +1,13 @@
+//syntax for if statement.
+
+
+// if(condition){
+//   //result
+// }
+
+
+let home_work= true; // I completed my homeowork.
+
+if(home_work){
+  console.log("I will watch a movie or play a game");
+}
