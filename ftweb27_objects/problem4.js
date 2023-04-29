bb71c142066277480c32a14fbc3b73e2bfb5f81e
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 02:46:44 +0530

    Some other problem has been done

---
+++ ftweb27_objects/problem4.js
@@ -0,0 +1,20 @@
+let details= {
+name: "goku",
+  Father_name: "gohan",
+  mother_name: "chichi",
+  address: "hyderabad",
+  age: 123,
+  pin_code: 500032,
+  married: false,
+  friend: "vegita"
+}
+
+//add into the object
+
+// dot notation
+details.hobbie= "playing pc games";
+
+//bracket notation
+details["enemy"]= "luffy"; 
+
+console.log(details.hobbie);
