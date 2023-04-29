ced6e6f89f8058d4020a6e195eb3b6156bbf97ce
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-04-30 02:55:37 +0530

    MY learning has done

---
+++ ftweb27_objects2/problem11.js
@@ -0,0 +1,11 @@
+let a= {
+  name:"goku",
+  age:23,
+  location: 'hyd',
+  hobbies: "fighting",
+  wife:"chichi"
+}
+
+console.log(Object.keys(a).length)//this  will give us the length of the object.
+
+
