fecb011a413d575573cb46be2b90a5b7f519cfe9
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-01 12:45:28 +0530

    Object method problem done

---
+++ ftweb27_object_method/problem2.js
@@ -0,0 +1,19 @@
+let details= {
+  name: "goku",
+  age: 21,
+  address: "delhi",
+  print: () => {
+    console.log(" this is a anonymous function")
+  }
+
+}
+
+details.print();// it is a way to call the function which is written inside the object.
+
+
+// details.name// goku
+// function print(){
+//   console.log(details.name);
+//   console.log(details.age);
+// }
+// print();
