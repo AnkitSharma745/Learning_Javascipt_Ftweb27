aef6fc7df194a4692da295466c3ba75b658ec22b
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-01 12:44:38 +0530

    Object method problem done

---
+++ ftweb27_object_method/problem1.js
@@ -0,0 +1,24 @@
+// 1. normal function. -->ES5
+
+// ES6----> 
+//2. anonymous function.
+//3. arrow function 
+
+//1.normal function
+function print(){
+  console.log("hello");
+}
+// print();
+
+//2. anonymous function.
+
+let trail= function (){
+  console.log("everyone");
+}
+// print1();
+
+//3. arrow function or anonymous arrow function.
+let print2=  () => {
+  console.log("hi")
+}
+print2();
