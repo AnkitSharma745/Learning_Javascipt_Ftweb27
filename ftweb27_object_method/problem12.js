86700ffd110e1a869bea5aaaf72ff8f35d9aa563
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-01 12:47:52 +0530

    Object method problem done

---
+++ ftweb27_object_method/problem12.js
@@ -0,0 +1,13 @@
+let object= {
+  data: [
+    {name: "iphone", price:80000, color:"red"},
+    {name: "oneplus", price:60000, color:"blue"},
+    {name: "redmi", price:50000, color:"yellow"},
+  ],
+  high_price : function(){
+    
+  }
+}
+
+// product, max---> iphone, 80000
+// home work 
