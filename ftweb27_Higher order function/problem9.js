0750699e2fed8507ebbe69b1b4835bc530e3ee40
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-01 13:01:17 +0530

    some problem done on Higher order function

---
+++ ftweb27_Higher order function/problem9.js
@@ -0,0 +1,15 @@
+//forEach--> array.
+
+let arr= [1,2,3,4];
+
+ arr.forEach(function (c,d){
+  // console.log(d);
+  // it cannot return any array;
+  console.log(c+1);
+});
+console.log(arr);
+
+//it is just used to iterate over the array.
+// it will not return anything.
+
+
