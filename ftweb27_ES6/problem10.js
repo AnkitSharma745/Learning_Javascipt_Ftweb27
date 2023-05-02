c257291dc13eeea1d059530f8bf954d31de72fb5
Ankit Sharma <ankitaksharma9763@gmail.com>
2023-05-02 10:22:05 +0530

    some ES6 program has been done

---
+++ ftweb27_ES6/problem10.js
@@ -0,0 +1,6 @@
+//rest operator --> it is used to convert arugments into array format.
+(...y)
+
+
+//spread operatro -->it is used to copy one array or object eleements into another array or object.
+  (...x)
---
+++ ftweb27_ES6/problem9.js
@@ -0,0 +1,11 @@
+let obj={
+  name: "luffy",
+  age: 12
+}
+
+let obj2={
+  address: "hyd",
+  ... obj
+}
+
+console.log(obj2);
